cac2bb19baa0ca5be6d0244f54dbead92b355c52
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-18 13:31:15 +0100

    Added xml file reading and parsing

--- src/PmdToJunit.ts
+++ src/PmdToJunit.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
 import * as xml from 'xml-js';
 import * as commander from 'commander';
+import * as path from 'path';
 
 commander.program.addOption(new commander.Option(
-    '-s, --source <string>', 
+    '-s, --source <string>',
     'Report file created by PMD'
 ).makeOptionMandatory());
 commander.program.addOption(new commander.Option(
@@ -20,7 +21,27 @@ commander.program.parse();
 async function main() {
     const options = commander.program.opts();
 
-    console.log(options);
+    const pathToSource = path.normalize(options['source']);
+    if (!fs.existsSync(pathToSource)) {
+        console.error('Source file doesn\'t exist!');
+        return;
+    }
+
+    const runPath = path.normalize(options['path']);
+    if (!fs.existsSync(runPath)) {
+        console.error('Run path doesn\'t exist!');
+        return;
+    }
+
+    const pathToOutput = path.normalize(options['output']);
+    if (fs.existsSync(pathToOutput)) {
+        console.error('Output file already exists!');
+        return;
+    }
+
+    const xmlOrgData = fs.readFileSync(pathToSource, 'utf8');
+
+    const jsonOrgData = xml.xml2js(xmlOrgData, { compact: true });
 }
 
 main()
